527afe1e66e214e10f97610cfa0752565fd78393
zhangsangousheng@gmail.com <zhangsangousheng@gmail.com>
2021-04-11 23:51:58 +0400

    first commit

---
+++ lib/utils/constants_page.dart
@@ -0,0 +1,35 @@
+class Constancts {
+  static const String SPLASH_NAME = '跳过';
+
+  static const String TAB_HOME = '首页';
+  static const String TAB_BOOK_VIDE = '书影音';
+  static const String TAB_GROUP = '小组';
+  static const String TAB_BAZAAR = '市集';
+  static const String TAB_ME = '我';
+
+  static const String CANNEL = '取消';
+  static const String OK = '确定';
+  static const String ALL = '全部';
+
+  //首页
+  static const String HOME_DYNAMIC = '动态';
+  static const String HOME_RECOMMEND = '推荐';
+  static const String HOME_SEAACH_TITLE = '我所见过的黄河';
+
+  //书影音
+  static const String BOOK_VIDEO_SEACH = '你生活中cozy的时刻';
+  static const String BOOK_VIDEO_VIDEO = '电影';
+  static const String BOOK_VIDEO_TV = '电视';
+  static const String BOOK_VIDEO_BOOK = '读书';
+  static const String BOOK_VIDEO_FIVCTION = '小说';
+  static const String BOOK_VIDEO_MUSIC = '音乐';
+  static const String BOOK_VIDEO_SAME_CITY = '同城';
+
+  static const String BOOK_VIDEO_FIND_video = '找电影';
+  static const String BOOK_VIDEO_LIST = '豆瓣榜单';
+  static const String BOOK_VIDEO_COMING_SOON = '即将上映';
+  static const String BOOK_VIDEO_TABLET_LIST = '豆瓣片单';
+
+  static const String BOOK_VIDEO_HOT_IN = '影院热映';
+  static const String BOOK_VIDEO_POPULAR = '影院热门';
+}
